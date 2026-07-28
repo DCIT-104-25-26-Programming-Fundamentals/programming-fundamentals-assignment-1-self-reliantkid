001e4b67cfc8553f3b1b1241dc5da0c036ca6d0a
self-reliantkid <syfolikumah@gmail.com>
2026-07-28 10:21:19 +0000

    feat: complete assignment03

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -39,3 +39,68 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+def sum_function(nums):
+    result = 0
+
+    for num in nums:
+        result += num
+
+    return result
+
+
+def average_function(nums):
+    num_sum = sum_function(nums)
+    result = num_sum/len(nums)
+    return result
+
+
+def max_function(nums):
+    result = nums[0]
+
+    for num in nums:
+        if num > result:
+            result = num
+
+    return result
+
+
+def min_function(nums):
+    result = nums[0]
+
+    for num in nums:
+        if num < result:
+            result = num
+
+    return result
+
+
+
+def main():
+    nums_list = []
+    num_count = int(input("How many numbers? "))
+    i = 1
+
+    while i <= num_count:
+        num = int(input(f"Enter number {i}: "))
+
+        if num <= 0:
+            print("Error: Enter a number greater than 0")
+            return
+        else:
+            nums_list.append(num)
+            i += 1
+
+    sum_result = sum_function(nums_list)
+    avg_result = average_function(nums_list)
+    max_result = max_function(nums_list)
+    min_result = min_function(nums_list)
+
+    print("\nResults:")
+    print("Sum:", sum_result)
+    print("Average:", avg_result)
+    print("Max:", max_result)
+    print("Min:", min_result)
+
+
+main()
