d2a2846a25cc7f134a277f07d64ea80e1fdfb6f7
self-reliantkid <syfolikumah@gmail.com>
2026-07-28 11:08:17 +0000

    feat: complete assignment08

--- assignment_08_student_records.py
+++ assignment_08_student_records.py
@@ -90,3 +90,111 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+students = []
+
+def add_student():
+    student = {}
+    try:
+        name = input("Student name: ")
+        id = input("Student ID: ")
+        no_score = int(input("How many scores? "))
+
+        if no_score <= 0:
+            print("Enter at least one score\n")
+            return
+
+        scores = []
+
+        for i in range(1, no_score+1):
+            score = int(input(f"Enter score {i}: "))
+            scores.append(score)
+
+        student["name"] = name
+        student["id"] = id
+        student["scores"] = scores
+
+        students.append(student)
+        print(f'Student "{name}" added successfully.\n\n')
+    except ValueError:
+        print("Invalid value!\n")
+
+
+def display_all():
+    if len(students) == 0:
+        print("No students have been added yet\n\n")
+    else:
+        print("--------------------------------------------------")
+        print(f"{'Name':<15}{'ID':<12}{'Scores':<15}{'Average'}")
+        print("--------------------------------------------------")
+
+        for student in students:
+            name = student["name"]
+            id = student["id"]
+            scores = ", ".join((str(sc) for sc in student["scores"]))
+
+            _, avg = calc_average(id)
+
+            print(f"{name:<15}{id:<12}{scores:<15}{avg}")
+
+        print("\n")
+
+
+def calc_average(student_id):
+    try:
+
+        student_details = next((student for student in students if student["id"] == student_id), None)
+
+        if student_details == None:
+            print("ID not found!")
+            return None
+        else:
+            name = student_details["name"]
+            scores = student_details["scores"]
+            avg = round(sum(scores)/len(scores), 2)
+
+            return name, avg
+            
+    except ValueError:
+        print("Invalid value!\n")
+
+
+def main():
+    while True:
+        print("================================")
+        print("   STUDENT RECORD SYSTEM MENU   ")
+        print("================================")
+        print("1. Add student")
+        print("2. Display all students")
+        print("3. Calculate average score")
+        print("4. Quit")
+
+        try: 
+            user_choice = int(input("\nEnter your choice (1-4): "))
+
+            if user_choice not in range(1, 5):
+                print("Option not in list!\n")
+            else:
+                if user_choice == 1:
+                    add_student()
+                elif user_choice == 2:
+                    display_all()
+                elif user_choice == 3:
+                    s_id = input("Enter student ID: ")
+                    result = calc_average(s_id)
+
+                    if result is None:
+                        print("ID not found\n")
+                    else:
+                        s_name, average = result
+                        print(f"{s_name}'s average score: {average:.2f}\n")
+                    
+                elif user_choice == 4:
+                    print("Goodbye!")
+                    return
+                
+        except ValueError:
+            print("Input must be a number in the options!\n")
+
+
+main()
